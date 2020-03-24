7c3b314de67945f1867cf6fe0f35ff1a04360944
maciek kanski <kanski@pop.pl>
2020-03-24 23:25:50 +0100

    make sure it somehow works

---
+++ src/Kanski.Fitting.Core/ProblemDomain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using GeneticSharp.Domain.Chromosomes;
+
+namespace Kanski.Fitting.Core
+{
+    public static class ProblemDomain
+    {
+        public static (double x, double y) ToPoint(this IChromosome chromosome)
+        {
+            Contract.Assert(null != chromosome);
+            var values = ((FloatingPointChromosome)chromosome).ToFloatingPoints();
+            Contract.Assert(2 == values.Length);
+            return (x: values[0], y: values[1]);
+        }
+
+        public static double Fitness(this IChromosome chromosome)
+        {
+            (var x, var y) = chromosome.ToPoint();
+            return Math.Exp((1 - x) * (x - 1)) + Math.Exp((1 - y) * (y - 1));
+        }
+
+    }
+}
--- src/Kanski.Fitting.Core/Sample.cs
+++ src/Kanski.Fitting.Core/Sample.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using GeneticSharp.Domain;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Fitnesses;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Domain.Terminations;
 
 namespace Kanski.Fitting.Core
 {
@@ -6,7 +14,41 @@ namespace Kanski.Fitting.Core
     {
         public static void Test()
         {
-            Console.WriteLine("Hello World!");
+            var selection = new RouletteWheelSelection();
+            var crossover = new UniformCrossover(0.5f);
+            var mutation = new FlipBitMutation();
+            var chromosome = new FloatingPointChromosome(
+                new double[] { 0, 0 },
+                new double[] { 2, 2 },
+                new int[] { 63, 63 },
+                new int[] { 17, 17 }
+            );
+            var population = new Population(500, 700, chromosome);
+            var fitness = new FuncFitness(ProblemDomain.Fitness);
+
+            var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
+            ga.Termination = new FitnessStagnationTermination(5000);
+
+            ga.MutationProbability = 0.1f;
+            ga.CrossoverProbability = 0.7f;
+
+
+            ga.GenerationRan += (s,a) =>
+            {
+                if (0 == ga.GenerationsNumber % 100)
+                {
+                    Console.WriteLine("Best solution so far {0} for {1}", ga.BestChromosome.Fitness, ga.BestChromosome.ToPoint());
+                }
+
+            };
+
+            Console.WriteLine("GA running...");
+            ga.Start();
+
+
+            Console.WriteLine("Best solution found has {0} fitness for {1}", ga.BestChromosome.Fitness, ga.BestChromosome.ToPoint());
+
         }
+
     }
 }
